644881c86c8b5868a86cb923089e61a1d7168e6f
= <=>
2021-09-01 22:11:12 +0700

    setting up config database reusable

--- helpers/config.js
+++ helpers/config.js
@@ -1,16 +1,25 @@
 const {Client} = require('pg')
 require("dotenv").config()
 
-const pg = new Client({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT || 3030,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+const pg  = (process.env.VERSION_STATE == 'production') ?
+    new Client({
+        host:process.env.DB_HOST,
+        port:process.env.DB_PORT || 3030,
+        user:process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database:process.env.DB_NAME,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })  
+    : 
+    new Client({
+        host:process.env.DB_HOST,
+        port:process.env.DB_PORT || 3030,
+        user:process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database:process.env.DB_NAME,
+    });
 
 pg.connect()
 .then(()=> console.log('Database Connect'))
